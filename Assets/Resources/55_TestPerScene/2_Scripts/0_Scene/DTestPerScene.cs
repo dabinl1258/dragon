03bc3c24b924436e3adcdec1a22f6bb2b614e345
dabinl1258 <ldbl1258@gmail.com>
2016-01-18 14:55:06 +0900

    제 저장

---
+++ Assets/Resources/55_TestPerScene/2_Scripts/0_Scene/DTestPerScene.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using MHomiLibrary;
+
+public class DTestPerScene: HState 
+{
+
+   public UILabel InfoLabel = null;
+
+   public override void Enter(params object[] oParams)
+   {
+
+   }
+
+   public override void Execute()
+   {
+
+   }
+
+   public override void Exit()
+   {
+
+   }
+}
---
+++ Assets/Resources/55_TestPerScene/2_Scripts/DInTestFlash.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DInTestFlash : MonoBehaviour {
+    DFlashingEffect flahs;
+	// Use this for initialization
+	void Start () {
+        flahs = GetComponent<DFlashingEffect>();
+        flahs.EnableEffect();
+        flahs.timerMax = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
