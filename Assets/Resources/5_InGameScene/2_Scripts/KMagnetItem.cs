d8ab3a95aabb9c898e64969bf5be7dbe267d3845
KimSungJu <tjdwn0802@naver.com>
2016-01-18 13:12:14 +0900

    자석아이템 추가

---
+++ Assets/Resources/5_InGameScene/2_Scripts/KMagnetItem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KMagnetItem : MonoBehaviour {
+
+    public static bool isActived = false;
+    public DPlayers Hero = null;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	    
+	}
+
+   
+
+    public void HitWithPlayer()
+    {
+        gameObject.SetActive(false);
+
+        if(isActived == false)
+            isActived = true;
+
+        Hero.OnMagnet();
+    }
+
+    void OnTriggerEnter(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            HitWithPlayer();
+        }
+    }
+
+}
---
+++ Assets/Resources/5_InGameScene/2_Scripts/KMagnetMng.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KMagnetMng : MonoBehaviour {
+    
+    public GameObject[] m_MagnetList = new GameObject[10];
+    int Headpointer = 0;
+    int Tailpointer = 0;
+    DPlayers m_Player;
+
+	// Use this for initialization
+	void Start () {
+        m_Player = transform.root.GetComponent<DPlayers>();
+
+        for(int i = 0 ; i < 10 ; i++ )
+        {
+            m_MagnetList[i] = null;
+        }
+        Debug.Log(m_MagnetList.Length);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(m_Player.getMagnet() == true)
+        {
+            if (Headpointer > 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (m_MagnetList[i] != null)
+                    {
+                        //m_MagnetList[i].transform.position = Vector2.Lerp(m_MagnetList[i].transform.position, transform.position, 1f);
+                        Debug.Log("magnet");
+                    }
+                }
+            }
+            
+        }
+        else
+        {
+             
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Item")== true)
+        {
+            Debug.Log("Item added");
+            AddObjectToArray(col.gameObject);
+        }
+    }
+
+    public void AddObjectToArray(GameObject p_gameobject)
+    {
+        m_MagnetList[Headpointer] = p_gameobject;
+        Headpointer++;
+    }
+
+    public void DelObjectInArray()
+    {
+        m_MagnetList[Tailpointer] = null;
+        Tailpointer++;
+    }
+}
+
+
